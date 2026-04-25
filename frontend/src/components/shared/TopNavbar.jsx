1bb1b6c36cbbd13fd898b5387d08cccdddeb62fa
vaibhav <borkarv908@gmail.com>
2026-04-25 18:02:08 +0530

    Updated TopNavbar changes

--- frontend/src/components/shared/TopNavbar.jsx
+++ frontend/src/components/shared/TopNavbar.jsx
@@ -11,25 +11,6 @@ function TopNavbar() {
 
   return (
     <>
-        <section className='w-full bg-[#08243a] text-white'>
-            <div className='flex items-center justify-center gap-2 text-sm'>
-                <div className='p-4 hover:bg-gray-400'>
-                    <button>India <PiCaretDown onClick={showDestinations} className='inline' /></button>
-                </div>
-                <div className='p-4 hover:bg-gray-400'>
-                    <button>World <PiCaretDown className='inline' /></button>
-                </div>
-                <div className='p-4 hover:bg-gray-400'>
-                    <button>Corporate Travel</button>
-                </div>
-                <div className='p-4 hover:bg-gray-400'>
-                    <button>Forex <PiCaretDown className='inline' /></button>
-                </div>
-                <div className='p-4 hover:bg-gray-400'>
-                    <button>Contact Us</button>
-                </div>
-            </div>
-        </section>
     </>
   )
 }
